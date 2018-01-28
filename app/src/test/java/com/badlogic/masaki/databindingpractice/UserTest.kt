8d1f638da5e35760b9187b8e28392bab508f030b
masaki shoji <dex10619@gmail.com>
2018-01-28 21:38:13 +0900

    Use Truth as assertion framework.

--- app/src/test/java/com/badlogic/masaki/databindingpractice/UserTest.kt
+++ app/src/test/java/com/badlogic/masaki/databindingpractice/UserTest.kt
@@ -1,42 +1,95 @@
 package com.badlogic.masaki.databindingpractice
 
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert.assertThat
+import com.google.common.truth.Truth.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.experimental.runners.Enclosed
+import org.junit.runner.RunWith
 
 /**
  * Created by shojimasaki on 2018/01/28.
  */
+@RunWith(Enclosed::class)
 class UserTest {
-    private var mSut: User? = null
+    class CaseThatTheOtherFieldIsNull {
+        private var mSut: User? = null
 
-    @Before
-    @Throws(Exception::class)
-    fun setUp() {
-        mSut = User()
+        @Before
+        @Throws(Exception::class)
+        fun setUp() {
+            mSut = User()
+        }
+
+        @After
+        @Throws(Exception::class)
+        fun tearDown() {
+            mSut = null
+        }
+
+        @Test
+        @Throws(Exception::class)
+        fun testUserFirstName() {
+            val expected = "firstName"
+            mSut?.firstName = "firstName"
+            assertThat(mSut?.firstName).isEqualTo(expected)
+            assertThat(mSut?.lastName).isNull()
+        }
+
+        @Test
+        @Throws(Exception::class)
+        fun testUserLastName() {
+            val expected = "lastName"
+            mSut?.lastName = "lastName"
+            assertThat(mSut?.firstName).isNull()
+            assertThat(mSut?.lastName).isEqualTo(expected)
+        }
     }
 
-    @After
-    @Throws(Exception::class)
-    fun tearDown() {
-        mSut = null
+    class CaseThatAllTheFieldsIsNotNull {
+        private var mSut: User? = null
+
+        @Before
+        @Throws(Exception::class)
+        fun setUp() {
+            mSut = User("firstName", "lastName")
+        }
+
+        @After
+        @Throws(Exception::class)
+        fun tearDown() {
+            mSut = null
+        }
+
+        @Test
+        @Throws(Exception::class)
+        fun testUser() {
+            val expectedUser = User(firstName = "firstName", lastName = "lastName")
+            assertThat(mSut?.firstName).isEqualTo(expectedUser.firstName)
+            assertThat(mSut?.lastName).isEqualTo(expectedUser.lastName)
+        }
     }
 
-    @Test
-    @Throws(Exception::class)
-    fun testUserFirstName() {
-        val expected = "firstName"
-        mSut?.firstName = "firstName"
-        assertThat(mSut?.firstName, `is`(expected))
-    }
+    class CaseThatAllTheFieldsIsNull {
+        private var mSut: User? = null
 
-    @Test
-    @Throws(Exception::class)
-    fun testUserLastName() {
-        val expected = "lastName"
-        mSut?.lastName = "lastName"
-        assertThat(mSut?.lastName, `is`(expected))
+        @Before
+        @Throws(Exception::class)
+        fun setUp() {
+            mSut = User()
+        }
+
+        @After
+        @Throws(Exception::class)
+        fun tearDown() {
+            mSut = null
+        }
+
+        @Test
+        @Throws(Exception::class)
+        fun testUser() {
+            assertThat(mSut?.firstName).isNull()
+            assertThat(mSut?.lastName).isNull()
+        }
     }
 }
